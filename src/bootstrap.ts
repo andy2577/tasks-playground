ec559b8a335798341a0271a895a2341791bc02b0
apetru <andriy.petrushchak@cgm.com>
2018-02-24 15:16:36 +0200

    added config files and mock data

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -2,20 +2,41 @@ import * as angular from 'angular';
 import { NgModuleDecorated } from 'angular-ts-decorators';
 import { OAuth } from './auth';
 import { AppModule } from './main';
+import  * as firebase  from 'firebase';
 
-const apiKey = 'AIzaSyDj08_wCkkDOBE0BwXI8poNZx08w_ViYS8';
-const clientId = '514763995828-compute@developer.gserviceaccount.com';
-const discoveryDocs = ['https://www.googleapis.com/discovery/v1/apis/tasks/v1/rest'];
-const scope = 'https://www.googleapis.com/auth/tasks';
+// const apiKey = 'AIzaSyDj08_wCkkDOBE0BwXI8poNZx08w_ViYS8';
+// const clientId = '514763995828-compute@developer.gserviceaccount.com';
+// const discoveryDocs = ['https://www.googleapis.com/discovery/v1/apis/tasks/v1/rest'];
+// const scope = 'https://www.googleapis.com/auth/tasks';
+
+// window['bootstrap'] = () => {
+//   const auth = new OAuth({ apiKey, discoveryDocs, clientId, scope});
+//   auth.on('gapi.auth2.isSignedIn', (isSignedIn) => {
+//     if (isSignedIn) {
+//       // bootstrap
+//       angular.element(document).ready(() => {
+//         angular.bootstrap(document, [(<NgModuleDecorated>AppModule).module.name], {strictDi: true});
+//       });
+//     }
+//   });
+// };
+
+const config = {
+  apiKey: 'AIzaSyBPfT512lf8kdEBNXCfbGMcJeIORCTCQfo',
+  authDomain: 'vlads-demo.firebaseapp.com',
+  databaseURL: 'https://vlads-demo.firebaseio.com',
+  projectId: 'vlads-demo',
+  storageBucket: 'vlads-demo.appspot.com',
+  messagingSenderId: '605069241132'
+};
+
+firebase.initializeApp(config);
+
+// const database = firebase.database();
 
 window['bootstrap'] = () => {
-  const auth = new OAuth({ apiKey, discoveryDocs, clientId, scope});
-  auth.on('gapi.auth2.isSignedIn', (isSignedIn) => {
-    if (isSignedIn) {
-      // bootstrap
-      angular.element(document).ready(() => {
-        angular.bootstrap(document, [(<NgModuleDecorated>AppModule).module.name], {strictDi: true});
-      });
-    }
+  // bootstrap
+  angular.element(document).ready(() => {
+    angular.bootstrap(document, [(<NgModuleDecorated>AppModule).module.name], {strictDi: true});
   });
 };
--- src/components/app.component.ts
+++ src/components/app.component.ts
@@ -1,8 +1,50 @@
 import { Component } from 'angular-ts-decorators';
+import  * as firebase  from 'firebase';
+
+// const config = {
+//   apiKey: 'AIzaSyBPfT512lf8kdEBNXCfbGMcJeIORCTCQfo',
+//   authDomain: 'vlads-demo.firebaseapp.com',
+//   databaseURL: 'https://vlads-demo.firebaseio.com',
+//   projectId: 'vlads-demo',
+//   storageBucket: 'vlads-demo.appspot.com',
+//   messagingSenderId: '605069241132'
+// };
+
+// firebase.initializeApp(config);
+
+// const DATABASE = firebase.database();
 
 @Component({
   selector: 'app',
   template: `<medical-form></medical-form>
+              <a
+                href="#"
+                ng-click="$ctrl.writeToDb()"
+                >write</a>
+              <a
+              href="#"
+              ng-click="$ctrl.readFromDb()"
+              >read</a>
               <ui-view></ui-view>`
 })
-export class AppComponent {}
+export class AppComponent {
+  DB;
+
+  constructor() {
+    this.DB  = firebase.database();
+  }
+
+  writeToDb() {
+    let newAppntKey = firebase.database().ref().child('appointments').push().key;
+    this.DB.ref('appointments/' + newAppntKey).set({dateFrom: 'Wed Feb 26 2018 13:45:47 GMT+0200 (FLE Standard Time)',
+    dateTo: 'Wed Feb 34 2018 13:45:47 GMT+0200 (FLE Standard Time)',
+    doctor: 'Cool Doctor'})
+      .then(() => console.log('Success'));
+    console.log('this is click');
+  }
+
+  readFromDb() {
+    this.DB.ref('appointments/').once('value')
+      .then(res => console.log('response', res.val()));
+  }
+}
