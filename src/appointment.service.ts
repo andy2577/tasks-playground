31a2f311d8f4f889d750651d9f0a4d74a6e29ac7
Andriy <andy25771@gmail.com>
2018-02-25 20:00:25 +0200

    added appointment.service

---
+++ src/appointment.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from 'angular-ts-decorators';
+import  * as firebase  from 'firebase';
+
+@Injectable('AppointmentSevice')
+export class AppointmentSevice {
+    DB;
+
+    constructor() {
+        this.DB  = firebase.database();
+    }
+
+    getAppointment() {
+        return 'test use';
+    }
+
+    writeToDb(appointment) {
+        let newAppntKey = firebase.database().ref().child('appointments').push().key;
+        return this.DB.ref('appointments/' + newAppntKey).set({...appointment, 'id': newAppntKey})
+    }
+
+    getAllAppointments() {
+        return this.DB.ref('appointments/').once('value')
+    }
+  
+    removeAppointment(appointmentId) {
+        return this.DB.ref('appointments/' + appointmentId).remove()
+    }
+ 
+}
+
