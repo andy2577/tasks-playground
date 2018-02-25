f59eacaa35cb4f36bb61cfc40a7883f249227a5b
Andriy <andy25771@gmail.com>
2018-02-25 20:16:06 +0200

    moved html to separete file

--- src/components/medical-form/medical-form.component.ts
+++ src/components/medical-form/medical-form.component.ts
@@ -2,67 +2,11 @@ import { Component } from 'angular-ts-decorators';
 import * as _ from 'lodash';
 import { AppointmentSevice } from '../../appointment.service';
 
+const template = require('./medical-form.component.html');
+
 @Component({
   selector: 'medicalForm',
-  template: `<div class="container">
-              <!-- Date Picker -->
-              <div class="row">
-                <div class="col-sm-3">
-                  <label name="datePickerFrom">From:</label>
-                </div>
-                <div class="col-sm-3">
-                  <date-picker
-                    selected-date="$ctrl.updateDate($event)"
-                    name="'dateFrom'" >
-                  </date-picker>
-                </div>
-                <div class="col-sm-3">
-                  <label name="datePickerTo">To:</label>
-                </div>
-                <div class="col-sm-3">
-                  <date-picker
-                    name="'dateTo'"
-                    min-date="$ctrl.minDate"
-                    selected-date="$ctrl.updateDate($event)">
-                  </date-picker>
-                </div>
-              </div>
-              <!-- End Date Picker -->
-
-              <!-- Drop Down -->
-                <div class="row">
-                  <drop-down
-                    items="$ctrl.itemsList"
-                    selected-doctor="$ctrl.updateDoctor($event)"
-                    ></drop-down>
-                </div>
-              <!-- End Drop Down -->
-              <!-- Buttons -->
-              <button type="button" 
-                class="btn btn-primary" 
-                ng-click="$ctrl.writeToDb()"
-              >
-              Save to DB
-              </button> 
-              <button type="button" 
-                class="btn btn-primary" 
-                ng-click="$ctrl.readFromDb()"
-              >Read</button>
-              <!-- End Buttons -->
-
-              <!-- Appointments -->
-              <h4 ng-show="$ctrl.appointmentList !== []" >Appointments:</h4>
-              <ul>
-                <li ng-repeat="appointment in $ctrl.appointmentList"  id="{{appointment.id}}" >
-                  <pre>
-{{ $index + 1 }} starts: {{ appointment.dateFrom }}
-  ends: {{ appointment.dateTo }}
-  doctor: {{ appointment.doctor }}
-                  </pre>
-                  <a href="#" ng-click="$ctrl.removeAppointment(appointment);">remove</a>
-                </li>
-              </ul>
-            </div>`
+  template
 })
 export class MedicalForm implements ng.IComponentController {
   minDate: Date = new Date('Wed Feb 1 2018 09:10:20 GMT+0200');
